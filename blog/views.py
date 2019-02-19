aa560546a61b82cf78e8869783d31429929d6a9e
Rvbens <ruben_onelove@hotmail.com>
2019-02-19 17:55:50 +0100

    first commit (settings ignored until sensitive data removed)

---
+++ blog/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+posts = [
+    {
+        "author":"Ruben",
+        "title":"Blog Post 1",
+        "content": "First post content",
+        "date_posted": "18 de Febrero"
+    },
+    {
+        "author":"Yoni",
+        "title":"Blog Post 2",
+        "content": "Second post content",
+        "date_posted": "19 de Febrero"
+    }
+]
+
+
+# Create your views here.
+def home(request):
+    context = {"posts":posts}
+    return render(request, "blog/home.html", context)
+
+def about(request):
+    return render(request, "blog/about.html",{"title":"About"})
