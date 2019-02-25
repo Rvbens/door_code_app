05dff1c85ab5b8b17b76926b05daa9ccb16fae56
Rvbens <ruben_onelove@hotmail.com>
2019-02-25 20:07:13 +0100

    Configured for heroku config vars

--- django_proyect/settings.py
+++ django_proyect/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-from .secret_settings import *
+#from .secret_settings import * # DEVELOPMENT
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
