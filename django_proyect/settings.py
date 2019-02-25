35838e0fe0e6019e3afb948714a494102a129901
Rvbens <ruben_onelove@hotmail.com>
2019-02-25 19:50:18 +0100

    Updated static root

--- django_proyect/settings.py
+++ django_proyect/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,"media")
@@ -132,3 +132,4 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST ='smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
