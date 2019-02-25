c45cb3038a341b5b3fb9d76fa4553db081c7e38f
Rvbens <ruben_onelove@hotmail.com>
2019-02-25 01:19:46 +0100

    Added password reset

--- django_proyect/settings.py
+++ django_proyect/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-from .secret_settings import SECRET_KEY
+from .secret_settings import *
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -127,3 +127,8 @@ CRISPY_TEMPLATE_PACK =  "bootstrap4"
 
 LOGIN_REDIRECT_URL = "blog-home"
 LOGIN_URL = "login"
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST ='smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
