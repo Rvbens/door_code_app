f8c932206864ddb241526ef928c31d3e98896d32
Rvbens <ruben_onelove@hotmail.com>
2019-02-27 22:06:37 +0100

    Remove create post function

--- blog/views.py
+++ blog/views.py
@@ -16,7 +16,7 @@ class PostListView(LoginRequiredMixin, ListView):
     ordering = ["-date_posted"]
     paginate_by = 5
 
-class UserPostListView(ListView):
+""" class UserPostListView(ListView):
     model = Post
     template_name = "blog/user_posts.html"
     context_object_name = "posts"
@@ -24,18 +24,18 @@ class UserPostListView(ListView):
 
     def get_queryset(self):
         user = get_object_or_404(User,username=self.kwargs.get("username"))
-        return Post.objects.filter(author=user).order_by("-date_posted")
+        return Post.objects.filter(author=user).order_by("-date_posted") """
 
 class PostDetailView(DetailView):
     model = Post
 
-class PostCreateView(LoginRequiredMixin, CreateView):
+""" class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ["title","content"]
 
     def form_valid(self, form):
         form.instance.author = self.request.user
-        return super().form_valid(form)
+        return super().form_valid(form) """
 
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
@@ -51,7 +51,7 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
             return True
         return False
 
-class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+""" class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     success_url = "/"
     
@@ -64,4 +64,4 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     
 
 def about(request):
-    return render(request, "blog/about.html",{"title":"About"})
+    return render(request, "blog/about.html",{"title":"About"}) """
